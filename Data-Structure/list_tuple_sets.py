6804d5720b77fca3d89ba5f82d050080921d5b6f
azzaky9 <zakiiws22@gmail.com>
2024-07-07 15:09:39 +0700

    chore: refactoring folder structure

---
+++ Data-Structure/list_tuple_sets.py
@@ -0,0 +1,9 @@
+
+# >> Lists Data Types for keep many data can access with index.
+
+# >> Tuples Similiar like a Lists but its constant
+
+# >> Dictionary Key-values pairs data keeper
+
+# >> Set keep your data Unique and Convert your duplicates data on another data types e.g List/Tuples
+
